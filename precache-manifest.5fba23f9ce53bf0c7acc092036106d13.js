c3b5f28efc6eaf29b323fc1e071375e1fa817660
Code Sandbox <hello@codesandbox.io>
2021-06-17 10:52:13 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.5fba23f9ce53bf0c7acc092036106d13.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae43db76ccbbdfee0b6d07ab64795d1f",
+    "url": "https://premreddy27042000.github.io/csb-3rfiw/index.html"
+  },
+  {
+    "revision": "a48f9d490ac6c260b2fd",
+    "url": "https://premreddy27042000.github.io/csb-3rfiw/static/js/2.41d2c938.chunk.js"
+  },
+  {
+    "revision": "d9028df2bbf341774622",
+    "url": "https://premreddy27042000.github.io/csb-3rfiw/static/js/main.96f13cf9.chunk.js"
+  },
+  {
+    "revision": "aab5d6140a26f7e881a3",
+    "url": "https://premreddy27042000.github.io/csb-3rfiw/static/js/runtime-main.eabcd0bd.js"
+  }
+]);
